1eabada917eaa120c2dabd48eb5a277955c6d340
whoogeek <rlarjswn0303@naver.com>
2024-11-06 09:29:35 +0900

    [TEST] 테스트 코드 추가

--- src/main/java/com/ohgiraffers/jenkins/service/CalculatorService.java
+++ src/main/java/com/ohgiraffers/jenkins/service/CalculatorService.java
@@ -1,9 +1,11 @@
 package com.ohgiraffers.jenkins.service;
 
 import com.ohgiraffers.jenkins.dto.CalculatorDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CalculatorService {
     public int plusTwoNumbers(CalculatorDTO calculatorDTO) {
         return calculatorDTO.getNum1() + calculatorDTO.getNum2();
